13b94ebdda72e45ab747501ce533d88594438c65
苏九 <65553800+644406669@users.noreply.github.com>
2023-10-02 12:03:12 +0800

    Update offset.h

--- offset.h
+++ offset.h
@@ -1,2 +1,8 @@
-	uint64_t GNames = 0xC2B6460;
-	uint64_t GObject = 0xC2BF460;
+uint64_t GNames = 0xC2B6460;
+uint64_t GObject = 0xC2BF460;
+ULONG64 GetGames(ULONG64 Names) {
+	auto Index = (Mem->Read<uint32_t>(Names) - 100) / 3 - 1;
+	auto name = Mem->Read<ULONG64>(Names + 0x8);
+	if (ValidPtr(name)) return 0;
+	return Mem->Read<uint64_t>(name + Index * 0x10);
+}
